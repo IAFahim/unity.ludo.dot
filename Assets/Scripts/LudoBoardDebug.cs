05db9dcc483d0b830a996d2ea6e8786c093d192e
IAFahim <ishtiaq.fahim@northsouth.edu>
2025-11-04 12:43:20 +0600

    Ludo init

---
+++ Assets/Scripts/LudoBoardDebug.cs
@@ -0,0 +1,36 @@
+using System;
+using Ludo;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class LudoBoardDebug : MonoBehaviour
+    {
+        public LudoBoard ludoBoard;
+
+        public int token;
+        public int dice;
+
+        [ContextMenu(nameof(Setup))]
+        public void Setup()
+        {
+        }
+
+        private void OnValidate()
+        {
+            var isTokenMoved = ludoBoard.TryMoveToken(token, dice, out byte position, out var error);
+            string logMessage = $"[{DateTime.Now:HH:mm:ss.fff}] " +
+                                $"Token: {token}, " +
+                                $"Dice: {dice}, " +
+                                $"Result: {(isTokenMoved ? "SUCCESS" : "FAILURE")}, ";
+
+            logMessage += ludoBoard.ToString();
+            if (!isTokenMoved)
+            {
+                logMessage += $", Error: {error}";
+            }
+
+            Debug.Log(logMessage);
+        }
+    }
+}
